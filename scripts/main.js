ded72e5f7da675bb682997c63d0152bef0dc6f46
Cristian Racedo <CristianDRacedo@hotmail.com>
2021-09-28 16:35:58 -0300

    Logica: Mapa de contacto

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,22 @@
 const d = document
 const menuBtn = d.querySelector('nav button')
 const menuLst = d.querySelector('nav .menu')
+const mapBtn = d.querySelector('.map button')
+const mapLst = d.querySelector('.map iframe')
 
 /*Barra de Navegacion */
-menuBtn.onclick = () => menuLst.classList.toggle('active') ? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
+menuBtn.onclick = menuLst.classList.toggle('active') ? 
+    menuBtn.innerHTML = '&times;' : 
+    menuBtn.innerHTML = '&equiv;'
 
 /*Mapa de contacto*/
+mapBtn.addEventListener('click', () => toggleMap() )
 
+const toggleMap = () => {
+    if(mapLst.classList.toggle('active')){
+        mapBtn.classList.replace('icon-max', 'icon-min')
+    } else {
+        mapBtn.classList.replace('icon-min', 'icon-max')
+    }
+}
 /* Extra */
