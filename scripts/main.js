bde4f458814213bb8c0e39d98c665559d6a8f724
Cristian Racedo <CristianDRacedo@hotmail.com>
2021-09-28 17:13:30 -0300

    Revision: Menu Hamburguesa

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,9 @@
 const d = document
 const menuBtn = d.querySelector('nav button')
 const menuLst = d.querySelector('nav .menu')
-const mapBtn = d.querySelector('.map button')
-const mapLst = d.querySelector('.map iframe')
 
 /*Barra de Navegacion */
-menuBtn.onclick = menuLst.classList.toggle('active') ? 
-    menuBtn.innerHTML = '&times;' : 
-    menuBtn.innerHTML = '&equiv;'
+menuBtn.onclick = () => menuLst.classList.toggle('active') ? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
 
 /*Mapa de contacto*/
 mapBtn.addEventListener('click', () => toggleMap() )
@@ -19,4 +15,3 @@ const toggleMap = () => {
         mapBtn.classList.replace('icon-min', 'icon-max')
     }
 }
-/* Extra */
