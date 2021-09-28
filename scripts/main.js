726187d7ace589980a89f58acc89c9339a56c248
Cristian Racedo <CristianDRacedo@hotmail.com>
2021-09-28 15:09:30 -0300

    Revision: Asignacion de Tareas

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,9 @@ const d = document
 const menuBtn = d.querySelector('nav button')
 const menuLst = d.querySelector('nav .menu')
 
+/*Barra de Navegacion */
 menuBtn.onclick = () => menuLst.classList.toggle('active') ? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
+
+/*Mapa de contacto*/
+
+/* Extra */
